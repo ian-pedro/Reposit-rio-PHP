b01c3218dbe1bfe5673c59be646b96ee1a2a446b
Ian Pedro <ianpeolno2003@gmail.com>
2020-07-05 09:42:07 -0300

    Salvando alterações na questão -> Sequência Secreta

--- Questoes/PROGRAMACAO/2019/FASE 1/Sequencia Secreta/PROGRAMACAO - FASE 1 - 2019_Sequencia Secreta.php
+++ Questoes/PROGRAMACAO/2019/FASE 1/Sequencia Secreta/PROGRAMACAO - FASE 1 - 2019_Sequencia Secreta.php
@@ -12,25 +12,25 @@
 */
 
 //Tamanho da sequência:
-$ts = 3;
+$ts = 5;
 //Sequência:
-$Vi = array(1,2,1);
+$Vi = array(1,1,1,2,1);
 
 if($ts>=3 && $ts<=500){
 
     if(count($Vi) == $ts){
 
         $arm = 0;
-        for($i=0; $i<=count($Vi); $i++){
+        $i = -1;
+        for($i; $i<=count($Vi); $i++){
+            echo $Vi[$i]."<br/>";
 
-            if($Vi[$i] != $i+1){
-                echo $arm = $Vi[$i];
-            }else{
-               return; 
+            if( $Vi[$i] != ($Vi[$i]+1) ){
+                $arm = $Vi[$i];
             }
 
         }
-
+        echo $arm;
     }else{
         echo "A sequência ultrapassa o tamanho";
     }
